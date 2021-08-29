eeb1a5cac15d28b57f0ac49d624272396f27e5b8
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2021-08-29 12:59:13 +0300

    fix modal window

--- src/blocks/modal-window/modal-window.js
+++ src/blocks/modal-window/modal-window.js
@@ -1,12 +1,21 @@
-let dataModal = document.querySelector('[data-modal-window]');
+let dataModals = document.querySelectorAll('[data-modal-window]');
 let modalWindow = document.querySelector('.modal-window');
 let nodeBody = document.querySelector('body');
+let dataModal;
 
-dataModal.addEventListener('click', function (e) {
-  e.preventDefault();
-  modalWindow.classList.add('modal-window_open');
-  nodeBody.classList.add('overflow-stop');
-});
+dataModals.forEach(
+  function dataModal( item ) {
+    clickEvent (item);
+  }
+);
+
+function clickEvent (item) {
+  item.addEventListener('click', function (e) {
+      e.preventDefault();
+      modalWindow.classList.add('modal-window_open');
+      nodeBody.classList.add('overflow-stop');
+    });
+}
 
 document.querySelector('.modal-window__close').addEventListener('click', function () {
   modalWindow.classList.remove('modal-window_open');
