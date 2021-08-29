8919f2c1420654ff4b9f6ab0935cd4adea963e28
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2021-08-29 23:21:52 +0300

    модалка

--- src/blocks/modal-window/modal-window.js
+++ src/blocks/modal-window/modal-window.js
@@ -1,17 +1,25 @@
 let dataModals = document.querySelectorAll('[data-modal-window]');
-let modalWindow = document.querySelector('.modal-window');
+let modalWindows = document.querySelectorAll('.modal-window');
 let nodeBody = document.querySelector('body');
+let modalClose = document.querySelectorAll('[data-modal-close]');
 
 dataModals.forEach( item => {
     item.addEventListener('click', (e) => {
       e.preventDefault();
+      let idModal = item.getAttribute('data-modal-window');
+      let modalWindow = document.getElementById(idModal);
       modalWindow.classList.add('modal-window_open');
       nodeBody.classList.add('overflow-stop');
     });
-  }
-);
+});
 
-document.querySelector('.modal-window__close').addEventListener('click', function () {
-  modalWindow.classList.remove('modal-window_open');
-  nodeBody.classList.remove('overflow-stop');
+
+
+modalClose.forEach(item => {
+    item.addEventListener('click', () => {
+      modalWindows.forEach(item => {
+        item.classList.remove('modal-window_open');
+      });
+      nodeBody.classList.remove('overflow-stop');
+    });
 });
